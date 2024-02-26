1e65ea95deb4e79bd4ecc125357aa14dce98c030
Vince Piotrowski <vinpiotrowski@gmail.com>
2024-02-25 18:18:01 -0700

    Use inline SVG version of logo

--- web/components/organisms/navigation.js
+++ web/components/organisms/navigation.js
@@ -1,7 +1,5 @@
-"use client"; // This is a client component
-
+'use client'; // This is a client component
 import React, { useState } from 'react'
-import Image from 'next/image'
 
 const Navigation = ({navigationContent}) => {
 
@@ -14,16 +12,18 @@ const Navigation = ({navigationContent}) => {
     return (
         <nav className='flex flex-wrap items-center justify-center m-auto max-w-6xl p-5 text-white relative z-20 lg:p-7'>
             <div className='flex items-center flex-shrink-0'>
-                <Image
-                    alt='' 
-                    src='/logo.png'
-                    height='70'
-                    width='40' />
-                <span className='ml-2 text-2xl lg:text-3xl'>vince<strong>piotrowski</strong></span>
+                <a href='/' className='flex items-center'>
+                    <svg width='42' height='49' style={{fill: 'rgba(53, 143, 170, 1)'}} xmlns='http://www.w3.org/2000/svg' viewBox='0 0 125 142'>
+                        <path d='M0,0v142h125V0H0ZM30.76,122.27l-4.03-8.03,7.19-14.48,3.85,7.82-7.01,14.7ZM20.06,63.28l-5.27,11.06h-7.64l12.74-26.98,27.94,55.59,27.59-55.74,4.01,7.69-31.55,63.74-27.82-55.35ZM82.04,104.71l-4.19-7h28.47l-32.42-62.45-27.63,55.6-3.96-7.82,31.41-63.32,44.12,84.98h-35.81Z'/>
+                    </svg>
+                    <span className='ml-2 text-2xl lg:text-3xl'>
+                        vince<strong>piotrowski</strong>
+                    </span>
+                </a>
             </div>
-            <div className="block flex flex-grow justify-end lg:hidden">
-                <button onClick={handleClick} className="flex items-center px-3 py-2">
-                    <svg className="fill-current h-5 w-5" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Menu</title><path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"/></svg>
+            <div className='block flex flex-grow justify-end lg:hidden'>
+                <button onClick={handleClick} className='flex items-center px-3 py-2'>
+                    <svg className='fill-current h-5 w-5' viewBox='0 0 20 20' xmlns='http://www.w3.org/2000/svg'><title>Open Menu</title><path d='M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z'/></svg>
                 </button>
             </div>
             <div className={`transition-all duration-300 ease-out block flex-grow items-center overflow-hidden w-full ${isOpen ? 'max-h-[300]' : 'max-h-0'} lg:flex lg:justify-end lg:max-h-none lg:w-auto`}>
