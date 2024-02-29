07c70731633a7e03461e77914d560fe480fb940d
Vince Piotrowski <vinpiotrowski@gmail.com>
2024-02-29 07:36:31 -0700

    Minor size revisions on heading and icon text size

--- web/components/molecules/icon-group.js
+++ web/components/molecules/icon-group.js
@@ -1,10 +1,13 @@
 const IconGroup = ({iconGroupContent, className}) => {
+
+    const textSizeStyle = iconGroupContent.length > 1 ? 'text-3xl lg:text-5xl' : 'text-5xl lg:text-7xl'
+
     return (
         <div className='flex flex-row justify-center items-center gap-2 text-center lg:gap-5'>
             {iconGroupContent.map((icon, index) => {
                 return (
                     <span 
-                        className={`icon-${icon?.icon} text-3xl lg:text-5xl ${className}`} 
+                        className={`icon-${icon?.icon} ${textSizeStyle} ${className}`} 
                         key={index} 
                         role='presentation' 
                         aria-hidden 
--- web/components/organisms/page-heading.js
+++ web/components/organisms/page-heading.js
@@ -4,9 +4,9 @@ import IconGroup from '../molecules/icon-group'
 const PageHeading = ({pageHeadingContent}) => {
     const {headline, iconGroup, intro, variant} = pageHeadingContent
 
-    const HeadingContentBlock = ({headline, iconGroup, intro, variant}) => {
+    const HeadingContentBlock = ({headline, iconGroup, intro, className}) => {
         return (
-            <div className='flex flex-col gap-3 max-w-xl text-center text-white lg:gap-7 lg:max-w-3xl'>
+            <div className={`flex flex-col gap-3 max-w-xl text-center text-white lg:gap-7 ${className}`}>
                 {iconGroup && (
                     <IconGroup iconGroupContent={iconGroup} />
                 )}
@@ -31,6 +31,7 @@ const PageHeading = ({pageHeadingContent}) => {
                     headline={headline}
                     iconGroup={iconGroup}
                     intro={intro}
+                    className='lg:max-w-3xl'
                 />
                 <div className='absolute bottom-0 h-1/2 right-0 w-full z-0 lg:h-3/4' role='presentation' aria-hidden>
                     <svg className='h-full w-full' preserveAspectRatio='none' viewBox='0 0 100 100'>
@@ -56,7 +57,7 @@ const PageHeading = ({pageHeadingContent}) => {
         )
     } else if (variant === 'pointy') {
         return (
-            <section className={`${variant} flex flex-col justify-center items-center relative pb-40 pt-14 px-7 text-white w-full lg:pb-56 lg:pt-28 lg:px-28`}>
+            <section className={`${variant} flex flex-col justify-center items-center relative pb-40 pt-14 px-7 text-white w-full lg:pb-56 lg:px-28`}>
                 <HeadingContentBlock 
                     headline={headline}
                     iconGroup={iconGroup}
