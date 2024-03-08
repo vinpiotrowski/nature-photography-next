dc0edbc3ecd37cc50a8859f39e8a5eaf7f0ba4d2
Vince Piotrowski <vinpiotrowski@gmail.com>
2024-03-07 17:15:44 -0700

    Removed root folder

--- web/next.config.js
+++ web/next.config.js
@@ -3,7 +3,6 @@ const {PHASE_PRODUCTION_BUILD} = require('next/constants')
 module.exports = (phase, {defaultConfig}) => {
     if(phase === PHASE_PRODUCTION_BUILD) {
         return {
-            basePath: '/nature-photography-next',
             output: 'export'
         }
     }
