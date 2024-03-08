3b4ee07403480f4a4aa280c57f36c4013b481a5e
Vince Piotrowski <vinpiotrowski@gmail.com>
2024-03-07 19:05:03 -0700

    Icon size tweaks

--- web/components/molecules/icon-group.js
+++ web/components/molecules/icon-group.js
@@ -1,11 +1,13 @@
 const IconGroup = ({iconGroupContent, className}) => {
- 
+
+    const textSizeStyle = iconGroupContent.length > 1 ? 'text-4xl md:text-6xl' : 'text-6xl lg:text-7xl'
+
     return (
         <div className={`flex flex-row gap-2 text-center lg:gap-3 ${className}`}>
             {iconGroupContent.map((icon, index) => {
                 return (
                     <span 
-                        className={`icon-${icon?.icon} text-4xl md:text-6xl`} 
+                        className={`icon-${icon?.icon} ${textSizeStyle}`} 
                         key={index} 
                         role='presentation' 
                         aria-hidden 
