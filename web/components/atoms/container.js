b7b2be49be41f290eccf67a5db2c222fdc1223ca
Vince Piotrowski <vinpiotrowski@gmail.com>
2024-11-24 08:59:13 -0700

    Container size adjustment

--- web/components/atoms/container.js
+++ web/components/atoms/container.js
@@ -1,7 +1,7 @@
 const Container = ({children, variant, className}) => {
     if(variant == 'nav') {
         return (
-            <div className={`max-w-screen-xl mx-auto px-7 py-2 lg:px-14 lg:py-5 ${className}`}>
+            <div className={`max-w-screen-xl mx-auto px-3 py-2 lg:px-14 lg:py-5 ${className}`}>
                 {children}
             </div>
         )
@@ -9,7 +9,7 @@ const Container = ({children, variant, className}) => {
 
     if(variant == 'pageHeading') {
         return (
-                <div className={`np-heading flex flex-col items-center w-full ${className}`}>
+                <div className={`np-heading flex flex-col px-3 items-center w-full lg:px-14 ${className}`}>
                     {children}
                 </div>
         )
@@ -17,14 +17,14 @@ const Container = ({children, variant, className}) => {
 
     if(variant == 'footer') {
         return (
-            <div className={`max-w-screen-xl mx-auto px-7 py-1 lg:px-14 lg:py-2 ${className}`}>
+            <div className={`max-w-screen-xl mx-auto px-3 py-1 lg:px-14 lg:py-2 ${className}`}>
                 {children}
             </div>
         )
     }
 
     return (
-        <div className={`max-w-screen-xl mx-auto p-7 z-20 lg:p-14 ${className}`}>
+        <div className={`max-w-screen-xl mx-auto px-5 py-7 z-20 lg:p-14 ${className}`}>
             {children}
         </div>
     )
