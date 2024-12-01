349184687b12e2fc0a06ccb8508b55de8d2120e3
Vince Piotrowski <vinpiotrowski@gmail.com>
2024-11-30 23:10:44 -0700

    Missed a case with the links scrolling

--- web/components/atoms/button.js
+++ web/components/atoms/button.js
@@ -9,7 +9,7 @@ const Button = ({ linkContent = {}, buttonVariant, onClick, className = ''}) => 
         case 'chonky':
             return (
                 <div className='flex justify-center py-1.5'>
-                    <NextLink href={href} className='np-button np-transition px-4 py-2.5 
+                    <NextLink href={href} scroll={false} className='np-button np-transition px-4 py-2.5 
                             z-10 sm:px-5 sm:py-3 hover:shadow-[0.7rem_0.7rem_0_rgba(255,0,108,1)]'>
                         <span className=' block font-bold sm:text-lg'>{text}</span>
                     </NextLink>
