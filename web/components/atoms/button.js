0fca164bbb1ef5a766ff75868b3f60a42fa257f2
Vince Piotrowski <vinpiotrowski@gmail.com>
2024-11-30 23:09:41 -0700

    Fix for links scrolling weird

--- web/components/atoms/button.js
+++ web/components/atoms/button.js
@@ -69,7 +69,7 @@ const Button = ({ linkContent = {}, buttonVariant, onClick, className = ''}) => 
         default:
             return (
                 <div className='flex'>
-                    <NextLink href={href} className='np-button np-transition px-3 py-2 
+                    <NextLink href={href} scroll={false} className='np-button np-transition px-3 py-2 
                              text-white z-10 lg:px-3 lg:py-2.5 hover:shadow-[0.7rem_0.7rem_0_rgba(255,0,108,1)]'>
                         <span className='block font-bold text-sm lg:text-base $className'>{text}</span>
                     </NextLink>
